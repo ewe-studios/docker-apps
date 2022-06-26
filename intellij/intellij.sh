695084a45fa6232f0555bbe2cef2a1214245ca7e
Alexander Ewetumo <trinoxf@gmail.com>
2022-06-26 16:39:29 +0800

    UPT: update docker

--- intellij/intellij.sh
+++ intellij/intellij.sh
@@ -4,7 +4,7 @@ sudo chown -R $PUID:$GUID /home/novnc/.idea
 sudo chown $PUID:$GUID "$IDEAU_HOME/bin/idea.sh"
 sudo chmod +x "$IDEAU_HOME/bin/idea.sh"
 
-#set -- gosu $PUID:$GUID sh "$IDEAU_HOME/bin/idea.sh" "$@"
-set -- sh "$IDEAU_HOME/bin/idea.sh" "$@"
+set -- gosu novnc sh "$IDEAU_HOME/bin/idea.sh" "$@"
+#set -- sh "$IDEAU_HOME/bin/idea.sh" "$@"
 
 exec "$@"
