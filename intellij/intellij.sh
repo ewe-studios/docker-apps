3dce58cf205a9a5b8f3300006e3c5c1d36b8ee5c
Alexander Ewetumo <trinoxf@gmail.com>
2022-06-26 16:36:55 +0800

    UPT: FIx starter

--- intellij/intellij.sh
+++ intellij/intellij.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 sudo chown -R $PUID:$GUID /home/novnc/.idea
-sudo chown  $PUID:$GUID /home/nonvc/Lab
 sudo chown $PUID:$GUID "$IDEAU_HOME/bin/idea.sh"
-
-sudo chmod -R 0777 /home/novnc/.idea
 sudo chmod +x "$IDEAU_HOME/bin/idea.sh"
 
-set -- gosu $PUID:$GUID sh "$IDEAU_HOME/bin/idea.sh" "$@"
+#set -- gosu $PUID:$GUID sh "$IDEAU_HOME/bin/idea.sh" "$@"
+set -- sh "$IDEAU_HOME/bin/idea.sh" "$@"
 
 exec "$@"
