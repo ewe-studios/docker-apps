93e066cfc6a82a0cf9d0071b97eaf79eaf3ccec3
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-02 20:25:31 +0800

    UPT: Add ssh addition

--- intellij/intellij.sh
+++ intellij/intellij.sh
@@ -2,9 +2,14 @@
 
 sudo chown -R $PUID:$GUID /home/novnc/.idea
 sudo chown $PUID:$GUID "$IDEAU_HOME/bin/idea.sh"
+sudo chown -R $PUID:$GUID "/home/novnc/.ssh"
 sudo chmod +x "$IDEAU_HOME/bin/idea.sh"
 
-#set -- sudo -H -u novnc sh "$IDEAU_HOME/bin/idea.sh" "$@"
+eval $(ssh-agent)
+
+if [ -f /home/novnc/.ssh/id_rsa ]; then
+  ssh-add /home/novnc/.ssh/id_rsa
+fi
 
 set -- sh "$IDEAU_HOME/bin/idea.sh" "$@"
 exec "$@"
