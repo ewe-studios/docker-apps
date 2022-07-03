3891ee89440d0c29e5e8d7c225ab97e9d27cdf0f
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-03 18:27:35 +0800

    UPT: Add crt & key

--- novnc/config/nobody/novnc.sh
+++ novnc/config/nobody/novnc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /home/novnc/noVNC/utils/novnc_proxy \
-  --cert /home/novnc/ssl/novnc.cert \
-  --key /home/novnc/ssl/novnc.key \
+  --cert /home/novnc/ssl/server.cert \
+  --key /home/novnc/ssl/server.key \
   --heartbeat 2 \
   --vnc localhost:5900 \
   --listen 6080
