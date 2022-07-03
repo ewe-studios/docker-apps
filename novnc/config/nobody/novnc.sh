8b8742cd6fc70f500e11e37984a8ba58e763fc08
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-03 18:56:42 +0800

    UPT: Add removal of tar

--- novnc/config/nobody/novnc.sh
+++ novnc/config/nobody/novnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /home/novnc/noVNC/utils/novnc_proxy \
-  --cert /home/novnc/ssl/server.cert \
+  --cert /home/novnc/ssl/server.crt \
   --key /home/novnc/ssl/server.key \
   --heartbeat 2 \
   --vnc localhost:5900 \
