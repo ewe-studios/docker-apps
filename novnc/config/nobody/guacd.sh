656b5094f2eee3cb152be91d3853dc992ee71f29
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-06 03:55:16 +0800

    UPT: Add guacd

---
+++ novnc/config/nobody/guacd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "${GUACA_SSL}" = "true" ]; then
+  guacd -f -l 5111 -b localhost -L debug -C /home/novnc/ssl/server.crt -K /home/novnc/ssl/server.key
+else
+  guacd -f -l 5111 -b localhost -L debug
+fi
