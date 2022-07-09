fd667759763010238e1a34ea6e6a79a77ae693d0
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-09 15:47:35 +0800

    UPT: Add config for 5 screens

--- novnc/config/nobody/x11_base.sh
+++ novnc/config/nobody/x11_base.sh
@@ -7,7 +7,12 @@ create_screen() {
   display="$1";
   screen="$2";
   size="$3";
-  opts="$4";
+
+  shift;
+  shift;
+  shift;
+
+  opts="$@";
 
   # setup env for user nobody
   echo "Xvfb ${display} -noreset -screen ${screen} ${size} ${opts}"
