400ea0d98718c74d45988be713c2280fc7a172f7
Alexander Ewetumo <trinoxf@gmail.com>
2022-07-09 13:36:47 +0800

    UPT: Add version change

--- novnc/config/nobody/vncconfig_1.sh
+++ novnc/config/nobody/vncconfig_1.sh
@@ -2,4 +2,4 @@
 
 DISPLAY=:0
 
-vncconfig -v -connect "0.0.0.0:$DISPLAY_1_PORT"
+vncconfig -v
--- novnc/config/nobody/vncconfig_2.sh
+++ novnc/config/nobody/vncconfig_2.sh
@@ -2,5 +2,5 @@
 
 if [ "${ENABLE_SCREEN_2}" = "1" ]; then
   DISPLAY=:1
-  vncconfig -v -connect "0.0.0.0:$DISPLAY_2_PORT"
+  vncconfig -v
 fi
